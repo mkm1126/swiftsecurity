25459ab730d6253458202b2b36468082aef5dc38
mkm1126 <jamie.kriegel@gmail.com>
2025-09-23 11:20:33 -0500

    Fix start_date null error in SelectRolesPage copy flow

--- src/SelectRolesPage.tsx
+++ src/SelectRolesPage.tsx
@@ -867,7 +867,7 @@ function SelectRolesPage() {
 
         // Create new request
         const requestPayload = {
-          start_date: formData.startDate,
+          start_date: d.startDate || new Date().toISOString().split('T')[0],
           employee_name: formData.employeeName,
           employee_id: formData.employeeId || null,
           is_non_employee: !!formData.isNonEmployee,
@@ -883,6 +883,8 @@ function SelectRolesPage() {
           supervisor_email: formData.supervisorUsername,
           security_admin_name: formData.securityAdminName,
           security_admin_email: formData.securityAdminUsername,
+          hr_mainframe_logon_id: d.hrMainframeLogonId || null,
+          hr_view_statewide: !!d.hrViewStatewide,
           status: 'pending',
           poc_user: pocUser,
         };
